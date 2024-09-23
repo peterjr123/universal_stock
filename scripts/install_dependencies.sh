0a9d757b91bd2c400dabfdcaa17501807db88ff0
peterjr123 <jsp020206@gmail.com>
2024-09-23 22:31:08 +0900

    export to root bashrc

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,5 @@
 echo install_dependencies
-export CATALINA_HOME=/home/ubuntu/apache-tomcat-10.1.20
-export DEPLOY_HOME=/home/ubuntu/deploy/
-export ARTIFACT_HOME=/home/ubuntu/artifacts/
+echo "export CATALINA_HOME=/home/ubuntu/apache-tomcat-10.1.20" >> /root/.bashrc
+echo "export DEPLOY_HOME=/home/ubuntu/deploy/" >> /root/.bashrc
+echo "export ARTIFACT_HOME=/home/ubuntu/artifacts/" >> /root/.bashrc
+source /root/.bashrc
